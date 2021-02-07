10d1b0a9e171b044b24e7d9b904d67b829aea255
dk-yoon <5097230@naver.com>
2021-02-07 23:02:48 +0900

    BOJ1010 - 다리 놓기 (수학, 다이나믹 프로그래밍, 조합론)

--- venv/BOJ/BOJ1010.py
+++ venv/BOJ/BOJ1010.py
@@ -2,6 +2,9 @@
 BOJ1010. 다리 놓기 (수학, 다이나믹 프로그래밍, 조합론)
 다리끼리는 서로 겹쳐질 수 없다고 할 때 다리를 지을 수 있는 경우의 수를 구하는 프로그램을 작성하라.
 '''
+
+# 처음에 시도하려 했던 것 = 전체 다리설치 가능 개수 - 조건을 불만족하는 다리 수 (교차되서 설치됐을 때)
+
 T = int(input())
 
 def factorial(a):
@@ -11,4 +14,6 @@ def factorial(a):
 
 for t in range (T):
     N, M = map(int, input().split(' '))
-    print(int(factorial(M)/((factorial(M-N))*(factorial(N)))))
+    print(int(factorial(M)/((factorial(M-N))*(factorial(N)))))  # nCr
+                                                                # nPr / r!
+                                                        # n! / ((n-r)! * (r!))
